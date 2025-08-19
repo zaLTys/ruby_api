3427e167249871705400755365bf40b6125ad851
Povilas Simanskas <povilas.simanskas@vinted.com>
2025-08-19 15:38:52 +0300

    env and stuff

--- app/controllers/api/v1/wiki_posts_controller.rb
+++ app/controllers/api/v1/wiki_posts_controller.rb
@@ -1,10 +1,15 @@
-
 # frozen_string_literal: true
 
 module Api
   module V1
-    class WikiPostsController < ApplicationController
-      skip_before_action :verify_authenticity_token
+    class WikiPostsController < ActionController::API
+      include ActionController::HttpAuthentication::Token::ControllerMethods
+      require 'csv'
+      # skip_before_action :verify_authenticity_token
+      before_action :authenticate
+
+      TOKEN = ENV['WIKI_API_KEY']
+
       include LoggingModule
 
       def show
@@ -12,14 +17,13 @@ module Api
         render json: @wiki_post
       end
 
-
       def create
         @wiki_post = WikiPost.new(wiki_post_params)
 
         if @wiki_post.save
           render json: @wiki_post, status: :created
         else
-          render json: @wiki_post.errors,status: :unprocessable_entity
+          render json: @wiki_post.errors, status: :unprocessable_entity
         end
       end
 
@@ -45,8 +49,16 @@ module Api
         render json: @wiki_posts
       end
 
-
-
+      def csv_index
+        @wiki_posts = WikiPost.all
+        csv_data = CSV.generate do |csv|
+          csv << ["ID", "Title", "Description", "Author"]
+          @wiki_posts.each do |post|
+            csv << [post.id, post.title, post.description, post.author]
+          end
+        end
+        send_data csv_data, filename: "wiki_posts.csv", type: "text/csv"
+      end
 
       # def index
       #   data = {}
@@ -68,6 +80,12 @@ module Api
 
       def wiki_post_params
         params.permit(:title, :description, :author)
+      end
+
+      def authenticate
+        authenticate_or_request_with_http_token do |token, options|
+          ActiveSupport::SecurityUtils.secure_compare(token, TOKEN)
+        end
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get 'wiki_posts/csv', to: 'wiki_posts#csv_index'
       resources :wiki_posts
     end
     namespace :v2 do
